2e9b0505160babdeb8c50c98f151c91c7da54feb
pokémféze <maximefeze1@gmail.com>
2023-07-03 13:05:47 +0100

    ajout de la page de demande de visa

--- Evisa/lib/main.dart
+++ Evisa/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:evisa/screen/home_screen.dart';
 import 'package:evisa/screen/signin_screen.dart';
+import 'package:evisa/screen/visaappliaction_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const VisaApplication(),
     );
   }
 }
--- Evisa/lib/screen/visaappliaction_screen.dart
+++ Evisa/lib/screen/visaappliaction_screen.dart
@@ -12,17 +12,143 @@ class VisaApplication extends StatefulWidget {
 class _VisaApplicationState extends State<VisaApplication> {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.indigo, // Couleur indigo pour la barre d'applications
+          title: const Text ("Visa Application"),
+          backgroundColor: Colors.indigo,
           leading: IconButton(
-            icon: const  Icon(Icons.arrow_back), // Icône de retour
+            icon: const Icon(Icons.arrow_back),
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen())); // Action à effectuer lors du clic sur le bouton de retour
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const HomeScreen()));
             },
           ),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(height: 15),
+            const Text(
+              "Informations",
+              style: TextStyle(fontSize: 25),
+            ),
+            const SizedBox(height: 5),
+            const Center(),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Divider(
+                thickness: 1,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.all(15),
+                children: [
+                  SizedBox(
+                    height: 50, // Hauteur souhaitée du bouton
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Logique du premier bouton
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white, // Couleur de fond du bouton (indigo)
+                        onPrimary: Colors.black87, // Couleur du texte sur le bouton (blanc)
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(color: Colors.white60), // Couleur des bordures (gris)
+                          borderRadius: BorderRadius.circular(5), // Rayon des coins des bordures
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start, // Alignement du contenu à gauche
+                        children: const [
+                          Icon(Icons.person, color: Colors.indigo,size: 30), // Icône utilisateur
+                          SizedBox(width:10), // Espacement entre l'icône et le texte
+                          Text("Information On The Applicant", style: TextStyle(fontSize: 18)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 50, // Hauteur souhaitée du bouton
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Logique du premier bouton
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white, // Couleur de fond du bouton (indigo)
+                        onPrimary: Colors.black87, // Couleur du texte sur le bouton (blanc)
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(color: Colors.white60), // Couleur des bordures (gris)
+                          borderRadius: BorderRadius.circular(5), // Rayon des coins des bordures
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start, // Alignement du contenu à gauche
+                        children: const [
+                          Icon(Icons.person, color: Colors.indigo,size: 30), // Icône utilisateur
+                          SizedBox(width:10), // Espacement entre l'icône et le texte
+                          Text("Information Document", style: TextStyle(fontSize: 18)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 50, // Hauteur souhaitée du bouton
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Logique du premier bouton
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white, // Couleur de fond du bouton (indigo)
+                        onPrimary: Colors.black87, // Couleur du texte sur le bouton (blanc)
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(color: Colors.white60), // Couleur des bordures (gris)
+                          borderRadius: BorderRadius.circular(5), // Rayon des coins des bordures
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start, // Alignement du contenu à gauche
+                        children: const [
+                          Icon(Icons.person, color: Colors.indigo,size: 30), // Icône utilisateur
+                          SizedBox(width:10), // Espacement entre l'icône et le texte
+                          Text("Information Prompter", style: TextStyle(fontSize: 18)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 50, // Hauteur souhaitée du bouton
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Logique du premier bouton
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white, // Couleur de fond du bouton (indigo)
+                        onPrimary: Colors.black87, // Couleur du texte sur le bouton (blanc)
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(color: Colors.white60), // Couleur des bordures (gris)
+                          borderRadius: BorderRadius.circular(5), // Rayon des coins des bordures
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start, // Alignement du contenu à gauche
+                        children: const [
+                          Icon(Icons.person, color: Colors.indigo,size: 30), // Icône utilisateur
+                          SizedBox(width:10), // Espacement entre l'icône et le texte
+                          Text("Information On The Visa", style: TextStyle(fontSize: 18)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
